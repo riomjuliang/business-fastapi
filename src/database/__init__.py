d7a948a1c136424f598656bff069a7d3f8d7fc25
Julian Gabriel Riom <julian.riom@mercadolibre.com>
2022-03-27 11:22:40 -0300

    Se configura BD de Heroku.

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -4,10 +4,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_USER = os.getenv("DB_USER", "jriom")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "55141055")
-POSTGRES_URL = os.getenv("POSTGRES_URL", "localhost:5432/fastapi_db")
-DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{POSTGRES_URL}"
+DB_USER = os.getenv("DB_USER", "vrgwlbtcblpeti")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "bacb7426ef9987b308a22e685cc9f963fbc4c5f223ea95eede289c2708a89755")
+POSTGRES_URL = os.getenv("POSTGRES_URL", "ec2-52-3-60-53.compute-1.amazonaws.com:5432/d6p1npmqrb6veh")
+DATABASE_URI = f"postgres://{DB_USER}:{DB_PASSWORD}@{POSTGRES_URL}"
 
 engine = create_engine(DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
