452b20e238cc969552d65788be6da75b5a566c01
Julian Gabriel Riom <julian.riom@mercadolibre.com>
2022-03-27 11:53:19 -0300

    Fix conexión DB.

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -4,13 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
-DATABASE_URI = "postgres://vrgwlbtcblpeti:bacb7426ef9987b308a22e685cc9f963fbc4c5f223ea95eede289c2708a89755@ec2-52-3" \
-               "-60-53.compute-1.amazonaws.com:5432/d6p1npmqrb6veh "
-
-if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
