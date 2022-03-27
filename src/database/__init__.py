087f9f02fb39cec8d62780ac33bfefa82efc7891
Julian Gabriel Riom <julian.riom@mercadolibre.com>
2022-03-27 13:29:08 -0300

    Fix DB.

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -6,6 +6,9 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
