5790918f25c5ee4e1b1703593f7f0351c265a78d
Julian Gabriel Riom <julian.riom@mercadolibre.com>
2022-03-28 19:42:08 -0300

    Se cambia a BD de Heroku.

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -5,12 +5,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
-#if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-#    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-DATABASE_URL = "postgresql://jriom:55141055@localhost/fastapi_db"
+## DATABASE_URL = "postgresql://jriom:55141055@localhost/fastapi_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
