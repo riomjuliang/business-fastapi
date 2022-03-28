9d5c4d29651cefe4f3a3d9040fdc750bd3048192
Julian Gabriel Riom <julian.riom@mercadolibre.com>
2022-03-28 17:47:56 -0300

    Se elimina archivo innecesario.

--- src/config/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def get_url():
-    user = os.getenv("POSTGRES_USER", "jriom")
-    password = os.getenv("POSTGRES_PASSWORD", "55141055")
-    server = os.getenv("POSTGRES_SERVER", "localhost:5432")
-    db = os.getenv("POSTGRES_DB", "fastapi_db")
-    return f"postgresql://{user}:{password}@{server}/{db}"
